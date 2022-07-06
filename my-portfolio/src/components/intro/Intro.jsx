7d54dfdc326ea49c6f55f108e1670048fc4cab69
Ineta-Kreiviene <ineta.kreiviene@icloud.com>
2022-07-06 20:07:07 +0300

    React text typing animation

--- my-portfolio/src/components/intro/Intro.jsx
+++ my-portfolio/src/components/intro/Intro.jsx
@@ -1,6 +1,19 @@
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
 import "./intro.scss";
 
 export default function Intro() {
+
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 500,
+      backSpeed: 80,
+      strings: ["Front-End Developer", "Front-End Designer", "Content Creator"]
+    })
+  }, []);
   return (
     <div className="intro" id="intro">
     <div className="left">
@@ -12,7 +25,7 @@ export default function Intro() {
       <div className="wrapper">
         <h2>Hi There, I'm</h2>
         <h1>Ineta Kreiviene</h1>
-        <h3>Junior Front-End <span>Developer</span></h3>
+        <h3>Junior <span ref={textRef}></span></h3>
       </div>
       <a href="#portfolio">
         <img src="assets/down.png" alt="arrow down" />
