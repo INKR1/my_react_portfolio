e2d15259abb0a4635cc60ba9c68bda141875dc93
Ineta-Kreiviene <ineta.kreiviene@icloud.com>
2022-07-06 18:19:13 +0300

    animated hamburger menu

--- my-portfolio/src/components/topbar/Topbar.jsx
+++ my-portfolio/src/components/topbar/Topbar.jsx
@@ -3,21 +3,25 @@ import {PersonOutline, AlternateEmail} from "@mui/icons-material";
 
 export default function Topbar() {
   return (
-    <div className="topbar">
+    <div className="topbar active">
       <div className="wrapper">
         <div className="left">
           <a href="#intro" className="logo">I.K.</a>
           <div className="itemContainer">
-          <PersonOutline className="icon"/>
-          <span>+370602019**</span>
+            <PersonOutline className="icon"/>
+            <span>+370602019**</span>
           </div>
           <div className="itemContainer">
-          <AlternateEmail className="icon"/>
-          <span>ineta.kreiviene@icloud.com</span>
+            <AlternateEmail className="icon"/>
+            <span>ineta.kreiviene@icloud.com</span>
           </div>
         </div>
         <div className="right">
-        this is right
+            <div className="hamburger">
+              <span className="line1"></span>
+              <span className="line2"></span>
+              <span className="line3"></span>
+            </div>
         </div>
       </div>
     </div>
