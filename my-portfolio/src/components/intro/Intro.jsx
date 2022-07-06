560d32f9543b149836635182265b537249446fd2
Ineta-Kreiviene <ineta.kreiviene@icloud.com>
2022-07-06 19:45:34 +0300

    react introduction page

--- my-portfolio/src/components/intro/Intro.jsx
+++ my-portfolio/src/components/intro/Intro.jsx
@@ -2,6 +2,22 @@ import "./intro.scss";
 
 export default function Intro() {
   return (
-   <div className="intro" id="intro"></div>
+    <div className="intro" id="intro">
+    <div className="left">
+      <div className="imgContainer">
+        <img src="assets/ProfilePic.jpg" alt="profile" />
+      </div>
+    </div>
+    <div className="right">
+      <div className="wrapper">
+        <h2>Hi There, I'm</h2>
+        <h1>Ineta Kreiviene</h1>
+        <h3>Junior Front-End <span>Developer</span></h3>
+      </div>
+      <a href="#portfolio">
+        <img src="assets/down.png" alt="arrow down" />
+      </a>
+    </div>
+  </div>
   )
 }
