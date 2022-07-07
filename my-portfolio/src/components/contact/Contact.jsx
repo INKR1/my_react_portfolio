7eb9f4afce45831ca52919a9a45c160ee239a636
Ineta-Kreiviene <ineta.kreiviene@icloud.com>
2022-07-07 17:58:07 +0300

    Created contacts setup

--- my-portfolio/src/components/contact/Contact.jsx
+++ my-portfolio/src/components/contact/Contact.jsx
@@ -2,7 +2,19 @@ import "./contact.scss";
 
 export default function Contact() {
   return (
-    <div className="contact" id="contact"></div>
+    <div className="contact" id="contact">
+      <div className="left">
+        <img src="assets/shake.svg" alt="" />
+      </div>
+      <div className="right">
+        <h2>Contact.</h2>
+        <form>
+          <input type="text" placeholder="Email" />
+          <textarea placeholder="Message"></textarea>
+          <button type="submit">Send</button>
+        </form>
+      </div>
+    </div>
     
   )
 }
