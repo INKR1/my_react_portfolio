9e03b4edc48fef78287dfcd00bb7fb61a7264d42
Ineta-Kreiviene <ineta.kreiviene@icloud.com>
2022-07-07 15:29:36 +0300

    Created testimonials setup

--- my-portfolio/src/components/testimonials/Testimonials.jsx
+++ my-portfolio/src/components/testimonials/Testimonials.jsx
@@ -2,6 +2,18 @@ import "./testimonials.scss";
 
 export default function Testimonials() {
   return (
-  <div className="testimonials" id="testimonials"></div>
+  <div className="testimonials" id="testimonials">
+    <h1>Testimonials</h1>
+    <div className="container">
+      <div className="card">
+        <div className="top">This is top and here wil be img</div>
+        <div className="center">This is Center and here wil be reviews</div>
+        <div className="bottom">This is bottom and here will be 
+          <h3>name</h3>
+          <h4>position</h4>
+        </div>
+      </div>
+    </div>
+  </div>
   )
 }
