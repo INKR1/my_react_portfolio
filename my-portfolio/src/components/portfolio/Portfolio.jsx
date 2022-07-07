b9228201fa8f5817c08584081800662f9befbd57
Ineta-Kreiviene <ineta.kreiviene@icloud.com>
2022-07-07 08:45:43 +0300

    React portfolio page

--- my-portfolio/src/components/portfolio/Portfolio.jsx
+++ my-portfolio/src/components/portfolio/Portfolio.jsx
@@ -1,7 +1,20 @@
 import "./portfolio.scss";
 
+
 export default function Portfolio() {
   return (
-   <div className="portfolio" id="portfolio"></div>
-  )
+    <div className="portfolio" id="portfolio">
+      <h1>Portfolio</h1>
+      <ul>
+       <li className="active">Web Development</li>
+       
+      </ul>
+      <div className="container">
+       <div className="item">
+        <img src="assets/book-store.png" alt="" />
+        <h3>Book Store</h3>
+       </div>
+      </div>
+    </div>
+  );
 }
