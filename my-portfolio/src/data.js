ca4b11e8aeb9f1e2d75a6a3e356afb699d7f90ff
Ineta-Kreiviene <ineta.kreiviene@icloud.com>
2022-07-07 10:08:43 +0300

    Added data file

---
+++ my-portfolio/src/data.js
@@ -0,0 +1,46 @@
+export const webDevPortfolio = [
+    {
+        id: 1,
+        title: "Book Store",
+        imgFile: "book-store.png", 
+    },
+    {
+        id: 2,
+        title: "Product Landing Page",
+        imgFile: "product-landing-page.png",
+    },
+    {
+        id: 3,
+        title: "Technical Documentation",
+        imgFile: "technical-documentation-page.png",
+    },
+    {
+        id: 4,
+        title: "Survey Form",
+        imgFile: "survey-form.png",
+    },
+    {
+        id: 5,
+        title: "Tribute Page",
+        imgFile: "tribute-page.png",
+    }
+
+];
+export const webAppPortfolio = [
+    {
+        id: 1,
+        title: "Calculator",
+        imgFile: "calculator.png",
+    },
+    {
+        id: 2,
+        title: "Weather Dashboard",
+        imgFile: "random-weather-generator.png",
+    },
+    {
+        id: 3,
+        title: "Business Card",
+        imgFile: "random-business-card-generator.png",
+    },
+
+];
