67e36c2d4f073182f783a51ee957cd0c046b1852
Ineta-Kreiviene <ineta.kreiviene@icloud.com>
2022-07-07 09:06:11 +0300

    Added portfolio list component

--- my-portfolio/src/components/portfolio/Portfolio.jsx
+++ my-portfolio/src/components/portfolio/Portfolio.jsx
@@ -1,13 +1,27 @@
 import "./portfolio.scss";
+import PortfolioList from "../portfolioList/PortfolioList";
+import { useState } from "react";
 
 
 export default function Portfolio() {
+  const [selected, setSelected] = useState("web dev");
+  const list = [
+    {
+      id: "web dev",
+      title: "Web Development"
+    }, 
+    {
+      id:"web app",
+      title: "Web Applications"
+    }
+  ];
   return (
     <div className="portfolio" id="portfolio">
       <h1>Portfolio</h1>
       <ul>
-       <li className="active">Web Development</li>
-       
+       {list.map((item)=>(
+        <PortfolioList key={item.id} title={item.title} active={selected === item.id} setSelected={setSelected}/>
+       ))}
       </ul>
       <div className="container">
        <div className="item">
